42a428110ab6725947f6c2693ba40c713339eab9
Sanadres <maycol_preciado@soy.sena.edu.co>
2025-06-25 12:24:04 -0500

    hola

--- Controlador/controlador.php
+++ Controlador/controlador.php
@@ -22,10 +22,8 @@ class Controlador
     }
     public function cerrarSesion()
     {
-        if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
-            unset($_SESSION['email']);
-            unset($_SESSION['password']);
-        }
+        unset($_SESSION['email']);
+        unset($_SESSION['password']);
         session_destroy();
         header("Location:index.php");
     }
